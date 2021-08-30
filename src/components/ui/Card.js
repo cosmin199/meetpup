56e171234e5f99288ac64ec4e430532524171bc0
cosmin199 <cosmin_ionescu_email@yahoo.ro>
2021-08-30 05:16:10 -0700

    form added

--- src/components/ui/Card.js
+++ src/components/ui/Card.js
@@ -1,5 +1,7 @@
 import classes from "./Card.module.css"
 
-export const Card = (props) => {
+function Card(props) {
   return <div className={classes.card}>{props.children}</div>
 }
+
+export default Card
