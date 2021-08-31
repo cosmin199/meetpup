aefcd179609063ba1e6773425e643fb94a224446
cosmin199 <cosmin_ionescu_email@yahoo.ro>
2021-08-31 06:42:47 -0700

    fetch data with useEffect hoock

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,29 @@
+import { useHistory } from "react-router-dom"
+
 import NewMeetupForm from "../components/meetups/NewMeetupForm"
 
 function NewMeetupPage() {
+  const history = useHistory()
+
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://meetup-6108e-default-rtdb.europe-west1.firebasedatabase.app/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then(() => {
+      history.replace("/")
+    })
+  }
+
   return (
     <section>
       <h1>add new meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   )
 }
