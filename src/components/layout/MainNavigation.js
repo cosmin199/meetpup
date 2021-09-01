3583f00570ac053629c37e663fc9e60a841d68c8
cosmin199 <cosmin_ionescu_email@yahoo.ro>
2021-08-31 23:20:04 -0700

    favorites features added

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,21 +1,30 @@
+import { useContext } from "react"
 import { Link } from "react-router-dom"
 
 import classes from "./MainNavigation.module.css"
+import FavoritesContext from "../../store/favorites-context"
 
 function MainNavigation() {
+  const favoritesCtx = useContext(FavoritesContext)
+
   return (
     <header className={classes.header}>
-      <div className={classes.logo}>react meetpus</div>
+      <div className={classes.logo}>React Meetups</div>
       <nav>
         <ul>
           <li>
-            <Link to="/">all meetups</Link>
+            <Link to="/">All Meetups</Link>
           </li>
           <li>
-            <Link to="/new-meetup">add new meetup</Link>
+            <Link to="/new-meetup">Add New Meetup</Link>
           </li>
           <li>
-            <Link to="/favorites">my favorites</Link>
+            <Link to="/favorites">
+              My Favorites
+              <span className={classes.badge}>
+                {favoritesCtx.totalFavorites}
+              </span>
+            </Link>
           </li>
         </ul>
       </nav>
